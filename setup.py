ba0fe521a7db766f70bac9302c9c86abc85cecb6
Alex Breslav <alexander.breslav@duke.edu>
2019-09-27 17:49:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='quicklook',
       version='0.1',
       description='A wrapper on matplotlib that facilitates fast/easy and attractive visualizations for exploratory data analysis.',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/alexdsbreslav/quicklook',
       author='Alexander Breslav',
       author_email='alexdsbreslav@gmail.com',
       packages =['quicklook'],
