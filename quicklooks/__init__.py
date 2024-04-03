5744c929e849537f2b4406d4f1d364553302640a
Alex Breslav <alexdsbreslav@gmail.com>
2024-04-03 15:58:56 -0400

    Update __init__.py

--- quicklooks/__init__.py
+++ quicklooks/__init__.py
@@ -3,26 +3,26 @@ PLACEHOLDER FOR INSTRUCTIONS
 '''
 
 # ---- chart skeleton
-from chart_skeleton import chart_skeleton
-from cs_attributes import chart_size
-from cs_attributes import chart_xlabel
-from cs_attributes import chart_ylabel
-from cs_attributes import color_libraries
-from cs_attributes import fonts
+from .chart_skeleton import chart_skeleton
+from .cs_attributes import chart_size
+from .cs_attributes import chart_xlabel
+from .cs_attributes import chart_ylabel
+from .cs_attributes import color_libraries
+from .cs_attributes import fonts
 
 # ---- plots
-from bar_plot import bar_plot
-from line_plot import line_plot
-from dist_plot import distribution_plot
-from scatter_plot import scatter_plot
+from .bar_plot import bar_plot
+from .line_plot import line_plot
+from .dist_plot import distribution_plot
+from .scatter_plot import scatter_plot
 
 # ---- reference items
-from reference_line import reference_line
-from chart_legend import legend
-from text import text
+from .reference_line import reference_line
+from .chart_legend import legend
+from .text import text
 
 # ---- templates
-from templates import templates
+from .templates import templates
 
 # ---- save function
-from save_chart import save_chart
+from .save_chart import save_chart
