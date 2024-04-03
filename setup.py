7dd19ecf12b1ecf6c4dabee05fb7965bec85a401
Alex Breslav <alexdsbreslav@gmail.com>
2024-04-03 13:58:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,30 @@
 from setuptools import setup
 
-setup(name='quicklook',
-      version='0.1',
-      description='A wrapper on matplotlib that facilitates fast/easy and attractive visualizations for exploratory data analysis.',
-      url='https://github.com/alexdsbreslav/quicklook',
-      author='Alexander Breslav',
-      author_email='alexdsbreslav@gmail.com',
-      packages =['quicklook'],
-      package_data = {'quicklook' : ['fonts/Futura.ttc', 'fonts/SourceSansPro-Black.ttf', 'fonts/SourceSansPro-Regular.ttf']},
-      install_requires = [
-        'numpy',
-        'matplotlib',
+setup(
+  name = 'quicklook',         # How you named your package folder (MyLib)
+  packages = ['quicklook'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'quicklook is a Python package for visualizing data quickly using matplotlib.',   # Give a short description about your library
+  author = 'Alex Breslav',                   # Type in your name
+  author_email = 'alexdsbreslav@gmail.com',      # Type in your E-Mail
+  url = 'https://www.linkedin.com/in/alexanderbreslav/',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['matplotlib', 'data-visualization', 'python'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'matplotlib',
+          'pandas',
+          'datetime',
+          'dateutil',
+          'seaborn'
       ],
-      include_package_data = True,
-      zip_safe=False)
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3.9',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+  ],
+)
